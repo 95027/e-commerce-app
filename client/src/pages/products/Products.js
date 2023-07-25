44f682d612fd7b2d1e36b8ca4d1da157ab7bf6bd
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-07-25 14:25:46 +0530

    ""

--- client/src/pages/products/Products.js
+++ client/src/pages/products/Products.js
@@ -2,6 +2,48 @@ import React from 'react'
 
 export const Products = () => {
   return (
-    <div>Products</div>
+    <div className='products'>
+      <div className="pleft">
+        <div className="filterItem">
+          <h2>Product categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" />
+            <label htmlFor="1">Shoes</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="2" />
+            <label htmlFor="2">Skirts</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="3" />
+            <label htmlFor="3">Coats</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" min={0} max={1000}/>
+            <span>1000</span>
+          </div>
+
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id="asc" value="asc" name='price'  />
+            <label htmlFor="asc">Price (lowest price)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id="desc" value="desc" name='price'  />
+            <label htmlFor="desc">Price (highest price)</label>
+          </div>
+        </div>
+      </div>
+      <div className="pright">
+        <img className="catImg" src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+      </div>
+
+    </div>
   )
 }
